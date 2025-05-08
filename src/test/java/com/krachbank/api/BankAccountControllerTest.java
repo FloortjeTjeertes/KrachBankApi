d4358389ebd5e9a5bbdd013f7448365fca6bf307
floortje tjeetes <florian.tjeertes@gmail.com>
2025-05-08 12:16:55 +0200

    ✅   add BankAccountController and corresponding test, remove TestController and ApiApplicationTests

---
+++ src/test/java/com/krachbank/api/BankAccountControllerTest.java
@@ -0,0 +1,33 @@
+package com.krachbank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.krachbank.api.controllers.BankAccountController;
+
+@WebMvcTest(BankAccountController.class)
+@AutoConfigureMockMvc
+public class BankAccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // @MockBean
+    // TestService TestService;
+
+    @Test
+    public void testPing() throws Exception {
+
+        mockMvc.perform(get("/ping"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("pong!"));
+    }
+
+}
