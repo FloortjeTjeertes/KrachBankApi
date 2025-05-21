3dd41f89401e9ff8f0a67e4670c437fafdefd377
Jochem Schippers <52064556+jochemschippers@users.noreply.github.com>
2025-05-21 14:58:41 +0200

    .

--- src/main/java/com/krachBank/api/repository/UserRepository.java
+++ src/main/java/com/krachBank/api/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.krachbank.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import com.krachbank.api.models.User;
