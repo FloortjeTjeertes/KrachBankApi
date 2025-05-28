5d999fbed5a13ff9e8999e416f6ce97d61d2e9f9
Jochem Schippers <52064556+jochemschippers@users.noreply.github.com>
2025-05-28 22:03:40 +0200

    fix

--- src/main/java/com/krachbank/api/security/JwtAuthenticationFilter.java
+++ src/main/java/com/krachbank/api/security/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+
 import java.io.IOException;
 
 @Component
@@ -27,12 +28,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         this.userDetailsService = userDetailsService;
     }
 
-    @Override // <--- This annotation was crucial and helps catch such errors during compilation
+    @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
-    ) throws ServletException, IOException { // <--- Added throws clause
+    ) throws ServletException, IOException { // <--- The correct throws clause is essential
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String username;
