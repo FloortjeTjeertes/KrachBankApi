c7cf06fe01f074dc6bf6dd9fae5cf8a232abcf8a
Floortje <florian.tjeertes@ziggo.nl>
2025-05-30 20:30:16 +0200

    ✨ feat: Update WebSecurityConfiguration to include Swagger UI and API docs in permitAll requests

--- src/main/java/com/krachbank/api/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/krachbank/api/configuration/WebSecurityConfiguration.java
@@ -10,21 +10,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-// We need to do this to allow POST requests
-    httpSecurity
-            .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/h2-console/**").permitAll() // allow h2-console
-                            // .anyRequest().authenticated()   // all other requests need authentication
-                            .anyRequest().permitAll()
-            )
-            .csrf(csrf -> csrf.disable())  //disable CSRF for h2-console
-            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())); // disable frame options for h2-console because of firefox 
-return httpSecurity.build();
-}
-@Bean
-public BCryptPasswordEncoder bCryptPasswordEncoder() {
-return new BCryptPasswordEncoder(12);
-}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // We need to do this to allow POST requests
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll() // allow h2-console
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html")
+                        // .anyRequest().authenticated()
+                        // all other requests need authentication
+                        .permitAll()
+                        .anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable()) // disable CSRF for h2-console
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())); // disable frame
+                                                                                                   // options for
+                                                                                                   // h2-console because
+                                                                                                   // of firefox
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 }
